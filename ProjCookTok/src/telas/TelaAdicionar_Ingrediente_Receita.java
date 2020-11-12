68280f35c8aa3363935dbddce15b6da8a74c3f86
Ester CardosoToja <63560521+estertoja@users.noreply.github.com>
2020-11-12 01:42:30 -0300

    Ajustes
    Foi adicionada uma nova classe  Ingrediente_Receita para poder adicionar os dados a tabela.
    Foi adicionado uma classe Enum para utilizar como unidade de medida em um combo box.

--- ProjCookTok/src/telas/TelaAdicionar_Ingrediente_Receita.java
+++ ProjCookTok/src/telas/TelaAdicionar_Ingrediente_Receita.java
@@ -369,6 +369,19 @@ public class TelaAdicionar_Ingrediente_Receita extends javax.swing.JFrame {
      */
     
     public static void main(String args[]) {
+        //MASSA PARA TESTES 
+        //INGREDIENTES
+        Ingrediente ingre1 = new Ingrediente("Leite Condensado");
+        Ingrediente ingre2 = new Ingrediente("Farinha de Trigo");
+        Ingrediente ingre3 = new Ingrediente("Leite");
+        Ingrediente ingre4 = new Ingrediente("Ovo");
+        Ingrediente ingre5 = new Ingrediente("Manteiga");
+        Ingrediente ingre6 = new Ingrediente("Oleo");
+        Ingrediente ingre7 = new Ingrediente("Granulado");
+        Ingrediente ingre8 = new Ingrediente("Creme de Leite");
+       // UNIDADE DE MEDIDA
+       Unidade_de_medida unidade_de_medida = new Unidade_de_Medida;
+               
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -392,6 +405,7 @@ public class TelaAdicionar_Ingrediente_Receita extends javax.swing.JFrame {
         }
         //</editor-fold>
 
+       
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
